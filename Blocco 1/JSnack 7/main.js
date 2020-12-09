f91781a3613d7187078c935259bbfa23a0ee14af
salvoab <salvatore.abate85@gmail.com>
2020-12-09 16:38:36 +0100

    Blocco1 JSnack7 completato

---
+++ Blocco 1/JSnack 7/main.js
@@ -0,0 +1,7 @@
+//Stampa le potenze di 2 fino a 1000. Lo interpreto fino all'esponente 1000
+
+const massimoEsponente = 1000;
+
+for(let esponente = 1; esponente <= massimoEsponente; esponente++){
+    console.log(`2^${esponente} = ${2**esponente}`);
+}
